71c25b39a73b46c9b1495a39c3e9723ba8fcaec8
FSSocrates <fssocrates@users.noreply.github.com>
2026-08-19 08:32:06 +0000

    Fix androidTest deps inside dependencies block

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,8 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.5.1"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
         release {
@@ -47,8 +48,6 @@ dependencies {
     implementation("com.jakewharton.timber:timber:5.0.1")
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
-}
-
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-
+}
